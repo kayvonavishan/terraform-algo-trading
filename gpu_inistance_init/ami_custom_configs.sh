2e3ee6f1ff9366547117fd494c7454a99dffd420
kayvonavishan <michaelkavishan@gmail.com>
2023-12-05 22:46:45 -0500

    adding ami configs

---
+++ gpu_inistance_init/ami_custom_configs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source activate pytorch
+conda config --set channel_priority flexible
+conda install -c conda-forge ta-lib plotly alpha_vantage ta pandasql itables jupyter
+sudo apt install -q -y nvme-cli
