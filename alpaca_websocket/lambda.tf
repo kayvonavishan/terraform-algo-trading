407e9202a9ef216d0776b1f785eaaf23e681be69
kayvonavishan <michaelkavishan@gmail.com>
2025-05-01 06:34:21 -0700

    test

--- alpaca_websocket/lambda.tf
+++ alpaca_websocket/lambda.tf
@@ -101,7 +101,7 @@ data "archive_file" "lambda_package" {
 resource "aws_lambda_function" "git_clone_lambda" {
   function_name = "GitCloneLambda"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "alpaca_websocket.lambda_function.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"  # Change to your desired Python runtime version
 
   #filename         = "deployment-package.zip"
