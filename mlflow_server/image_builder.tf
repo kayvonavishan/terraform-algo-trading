7113d5336ce56bc3edcadef0a2ce99f52a14cc79
kayvonavishan <michaelkavishan@gmail.com>
2025-06-23 23:42:59 -0700

    update

--- mlflow_server/image_builder.tf
+++ mlflow_server/image_builder.tf
@@ -74,14 +74,7 @@ resource "aws_security_group" "builder" {
   }
 }
 
-data "aws_iam_policy_document" "assume_ec2" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
   }
 }
 
