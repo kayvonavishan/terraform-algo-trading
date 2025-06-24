8636c5f2408428d0b33372fac7e937aa1807a8f4
kayvonavishan <michaelkavishan@gmail.com>
2025-06-24 00:18:43 -0700

    update

--- mlflow_server/iam.tf
+++ mlflow_server/iam.tf
@@ -39,3 +39,8 @@ resource "aws_iam_instance_profile" "ec2" {
   name = "mlflow-ec2-profile"
   role = aws_iam_role.ec2.name
 }
+
+resource "aws_iam_role_policy_attachment" "imagebuilder_readonly" {
+  role       = aws_iam_role.imagebuilder.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSImageBuilderReadOnlyAccess"
+}
