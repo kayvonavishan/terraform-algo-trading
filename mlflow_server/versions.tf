5bf2004063ed1097ebf490315b29f03b0dfd68b5
kayvonavishan <michaelkavishan@gmail.com>
2025-06-23 23:36:15 -0700

    test

--- mlflow_server/versions.tf
+++ mlflow_server/versions.tf
@@ -1,10 +1,11 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  required_providers {
-    aws      = { source = "hashicorp/aws",      version = ">= 5.90" }
+    required_providers {
+    aws    = { source = "hashicorp/aws",    version = ">= 5.90" }
+    random = { source = "hashicorp/random", version = "~> 3.7" }
+  }
     random   = { source = "hashicorp/random",   version = "~> 3.6" }
-    template = { source = "hashicorp/template", version = "~> 2.3" }
   }
 }
 
