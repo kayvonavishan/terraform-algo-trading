4c56a6d1734a52ca3c4becfac7636bda4b204026
kayvonavishan <michaelkavishan@gmail.com>
2025-07-14 20:52:29 -0400

    Update run.sh

--- trading_server/run.sh
+++ trading_server/run.sh
@@ -30,7 +30,7 @@ if [ ! -d "algo-modeling-v2" ]; then
   sudo chown -R ec2-user:ec2-user /home/ec2-user/algo-modeling-v2
   cd algo-modeling-v2
   sudo -u ec2-user git fetch > /home/ec2-user/live_trader.log
-  sudo -u ec2-user git checkout feature/deployment-final-v2 >> /home/ec2-user/live_trader.log
+  sudo -u ec2-user git checkout feature/wire-streaming-val >> /home/ec2-user/live_trader.log
   sudo -u ec2-user git pull >> /home/ec2-user/live_trader.log
 else
   echo "Repository exists. Pulling latest changes..."
@@ -40,7 +40,7 @@ else
   sudo chown -R ec2-user:ec2-user /home/ec2-user/algo-modeling-v2
   cd algo-modeling-v2
   sudo -u ec2-user git fetch >> /home/ec2-user/live_trader.log
-  sudo -u ec2-user git checkout feature/deployment-final-v2 >> /home/ec2-user/live_trader.log
+  sudo -u ec2-user git checkout feature/wire-streaming-val >> /home/ec2-user/live_trader.log
   sudo -u ec2-user git pull >> /home/ec2-user/live_trader.log
 fi
 
